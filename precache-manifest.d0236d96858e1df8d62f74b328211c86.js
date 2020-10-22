c9ddbb69e7d11d146ae285e76d9f479e66fe026b
Miron Gindis <mge1999@yaqndex.ru>
2020-10-22 23:23:09 +0500

    Updates

---
+++ precache-manifest.d0236d96858e1df8d62f74b328211c86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd75d7346e490de7b499f1bd92e5856",
+    "url": "/site2/index.html"
+  },
+  {
+    "revision": "40be0ae86cc7f396e945",
+    "url": "/site2/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "66761f2b607c39742ab3",
+    "url": "/site2/static/css/main.064747ad.chunk.css"
+  },
+  {
+    "revision": "40be0ae86cc7f396e945",
+    "url": "/site2/static/js/2.1038b9a2.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/site2/static/js/2.1038b9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66761f2b607c39742ab3",
+    "url": "/site2/static/js/main.3a6f94d0.chunk.js"
+  },
+  {
+    "revision": "90853949b581c26ca119",
+    "url": "/site2/static/js/runtime-main.53c5d823.js"
+  },
+  {
+    "revision": "e6b73ea0fcab3a999f7cb2d51daa75a8",
+    "url": "/site2/static/media/footer.e6b73ea0.webp"
+  },
+  {
+    "revision": "aea24c33bae5d535877c83b254229526",
+    "url": "/site2/static/media/name.aea24c33.svg"
+  },
+  {
+    "revision": "c35659dba9a7a698a190241e4ccdc96d",
+    "url": "/site2/static/media/photo.c35659db.webp"
+  }
+]);
